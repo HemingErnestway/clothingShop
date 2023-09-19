169f9f9c975270f9670dd718ac73aabe7bfd6727
Heming Ernestway <ernestwayh@gmail.com>
2023-09-19 23:08:19 +0300

    created instances for each Product's property match the api

--- main.go
+++ main.go
@@ -31,22 +31,57 @@ func main() {
 	}
 	fmt.Printf("User %s\n\n", string(userJSON))
 
-	//TODO: think about linking properties!
-	//product := entities.Product{
-	//	Uuid:         uuid.New(),
-	//	Category:     "Обувь",
-	//	Color:        "Черный",
-	//	Season:       "Лето",
-	//	Size:         "44",
-	//	Manufacturer: "Россия",
-	//	Brand:        "Adidas",
-	//	Gender:       entities.Male,
-	//	Age:          "Adult",
-	//	PriceRoubles: 3299.90,
-	//}
-	//productJSON, err := json.MarshalIndent(product, "", "  ")
-	//if err != nil {
-	//	log.Fatalf(err.Error())
-	//}
-	//fmt.Printf("Product %s\n\n", string(productJSON))
+	shoesCategory := entities.Category{
+		Uuid: uuid.New(),
+		Name: "Обувь",
+	}
+
+	blackColor := entities.Color{
+		Uuid: uuid.New(),
+		Name: "black",
+	}
+
+	summerSeason := entities.Season{
+		Uuid: uuid.New(),
+		Name: "Лето",
+	}
+
+	fourtyFourSize := entities.Size{
+		Uuid: uuid.New(),
+		Name: "44",
+	}
+
+	russiaManufacturer := entities.Manufacturer{
+		Uuid: uuid.New(),
+		Name: "Россия",
+	}
+
+	adidasBrand := entities.Brand{
+		Uuid: uuid.New(),
+		Name: "Adidas",
+	}
+
+	adultAgeGroup := entities.AgeGroup{
+		Uuid: uuid.New(),
+		Name: "Adult",
+	}
+
+	product := entities.Product{
+		Uuid:           uuid.New(),
+		CategoryId:     shoesCategory.Uuid,
+		ColorId:        blackColor.Uuid,
+		SeasonId:       summerSeason.Uuid,
+		SizeId:         fourtyFourSize.Uuid,
+		ManufacturerId: russiaManufacturer.Uuid,
+		BrandId:        adidasBrand.Uuid,
+		Gender:         entities.Male,
+		AgeGroupId:     adultAgeGroup.Uuid,
+		PriceRoubles:   3299.90,
+	}
+	productJSON, err := json.MarshalIndent(product, "", "  ")
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	fmt.Printf("Product %s\n\n", string(productJSON))
+	fmt.Println()
 }
