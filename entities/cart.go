bd196ca448e1d91ff5573e06c52ac8e89339c618
Sa1Ko-inv <dmitriyblinow@mail.ru>
2023-09-30 09:30:09 +0300

    Added Cart struct and empty methods for Handler

---
+++ entities/cart.go
@@ -0,0 +1,14 @@
+package entities
+
+import "github.com/google/uuid"
+
+type CartItem struct {
+	ProductId        uuid.UUID
+	Quantity         uint32
+	ItemPriceRoubles float64
+}
+
+type Cart struct {
+	Items             []CartItem
+	TotalPriceRoubles float64
+}
